fc198102de2f7d6601051ea060518e0b1b1cfaad
Antonio J. Delgado <ad@susurrando.com>
2022-05-05 16:50:05 +0300

    fix installer

--- install_my_bashrc.sh
+++ install_my_bashrc.sh
@@ -5,5 +5,6 @@ my_dir=$(dirname "${0}")
 if [ -e "${HOME}/.bashrc" ]; then
     mv "${HOME}/.bashrc" "${HOME}/.bashrc.bak"
 fi
-cp "${my_dir}/bash_rc" "${HOME}/.bashrc" -rfp
-cp "${my_dir}/bashrc.d" "${HOME}/.bashrc.d" -rfp
+cd "${my_dir}" ||exit 1
+cp bash_rc "${HOME}/.bashrc" -rfp
+cp bashrc.d/* "${HOME}/.bashrc.d/" -rfp
