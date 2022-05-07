8a3e194140926fe12ef55a434fe3066fb7b80c82
Antonio J. Delgado <ad@susurrando.com>
2022-05-07 12:16:52 +0300

    Update readme, add sources and move some things to the folder

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # my_bashrc
-My Bash RC file
+
+My Bash RC file. With the posibility to add more files from a ~/.bashrc.d folder and to install more, from git repositories (in the *sources* file).
+
+# Installation
+
+Run install_my_bashrc.sh as the user you want to install it, or give an existing folder as parameter to install it somewhere else (/etc/skel for example).
--- install_my_bashrc.sh
+++ install_my_bashrc.sh
@@ -2,10 +2,32 @@
 
 my_dir=$(dirname "${0}")
 
-if [ -e "${HOME}/.bashrc" ]; then
-    mv "${HOME}/.bashrc" "${HOME}/.bashrc.bak"
+if [ -n "${1}" ]; then
+    install_directory="${1}"
+    if [ ! -d "${install_directory}" ]; then
+        echo "The given install directory '${install_directory}' does NOT exists."
+        exit 2
+    fi
+    echo "Installing in '${install_directory}' folder instead of \$HOME"
+else
+    install_directory="${HOME}"
+fi
+
+if [ -e "${install_directory}/.bashrc" ]; then
+    mv "${install_directory}/.bashrc" "${install_directory}/.bashrc.bak"
 fi
 cd "${my_dir}" ||exit 1
-cp bash_rc "${HOME}/.bashrc" -rfp
-mkdir -p "${HOME}/.bashrc.d/"
-cp bashrc.d/* "${HOME}/.bashrc.d/" -rfp
+cp bash_rc "${install_directory}/.bashrc" -rfp
+mkdir -p "${install_directory}/.bashrc.d/"
+cp bashrc.d/* "${install_directory}/.bashrc.d/" -rfp
+if [ -e "${my_dir}/sources}" ]; then
+    mkdir -p "${install_directory}/src/"
+    while read -r source
+    do
+        source_name=$(basename "${source}" .git)
+        git clone "${source}" "${install_directory}/src/${source_name}" --depth 1
+        if [ -e "${install_directory}/src/${source_name}/install.sh" ]; then
+            bash "${install_directory}/src/${source_name}/install.sh"
+        fi
+    done <<< "$(sed 's/#.*$//g' "${my_dir}/sources")"
+fi
